b72701b37acc927c6b28e6588f5193de03bb5fe6
xarzoa <xarzoa@gmail.com>
2024-04-19 23:37:07 +0530

    resend

--- app/auth/action.js
+++ app/auth/action.js
@@ -1,6 +1,6 @@
 'use server';
 import { z } from 'zod';
-import { signIn, auth } from '@/lib/auth';
+import { signIn } from '@/lib/auth';
 
 const FormSchema = z.object({
   email: z.string().email(),
@@ -18,8 +18,8 @@ export async function signInAction(data) {
     };
   }
   try {
-    await signIn('nodemailer', { email: data.email, redirect: false })
-    // await signIn('resend', { email: data.email, redirect: false })
+    // await signIn('nodemailer', { email: data.email, redirect: false })
+    await signIn('resend', { email: data.email, redirect: false })
     return {
       message: 'We sent you the magic-link. Check your spams too.',
       type: 'success',
